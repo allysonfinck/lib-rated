634a905307a347d32b85161ecf1f97ca2cd4ef0f
Allyson Finck <allyson.finck@gmail.com>
2018-04-25 07:12:43 -0400

    rails app setup minus model data

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  post '/users', to: 'users#create'
+  delete '/users/:id', to: 'users#delete'
+  put '/users/:id', to: 'users#update'
+
+  get '/books', to: 'books#index'
+  get '/books/:id', to: 'books#show'
+  post '/books', to: 'books#create'
+  delete '/books/:id', to: 'books#delete'
+  put '/books/:id', to: 'books#update'
+
+  get '/favorites', to: 'favorites#index'
+  get '/favorites/:id', to: 'favorites#show'
+  post '/favorites', to: 'favorites#create'
+  delete '/favorites/:id', to: 'favorites#delete'
+  put '/favorites/:id', to: 'favorites#update'
+end
