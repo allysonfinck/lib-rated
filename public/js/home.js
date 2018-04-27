78221b2b48601c6a5a7b27729acd6604cb204045
Eric Sanchez <bluemojo82@gmail.com>
2018-04-27 17:28:54 -0400

    search functionality working (with minor bugs)

--- public/js/home.js
+++ public/js/home.js
@@ -1,82 +1,39 @@
 
 class SearchResult extends React.Component {
   render() {
+    // console.log(this.props.googleBooks);
     return (
-      <div>
-        {/* Demo Result */}
-        <div className="col s12 m7">
-          <h3 className="header">Book Title</h3>
-          <div className="card horizontal cardfave">
-            <div className="card-image">
-              <img src="https://upload.wikimedia.org/wikipedia/en/thumb/6/6f/Stephen_King_Misery_cover.jpg/220px-Stephen_King_Misery_cover.jpg" />
-            </div>
-            <div className="card-stacked">
-              <div className="card-content">
-                <p>Plot: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde nesciunt minus ab animi autem sed modi perspiciatis a, deserunt magnam, provident ut nam quis minima aspernatur repudiandae iusto quam fugit.</p>
-                <ul>
-                  <li>Author: Stephen King</li>
-                  <li>Publisher: Viking</li>
-                  <li>Genre: Psychological Horror</li>
-                  <li>Date Published: 1987</li>
-                  <li>Pages: 420</li>
-                </ul>
-                <span>Rating: 5</span>
-              </div>
-              <div className="card-action">
-                <a>This is a link</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col s12 m7">
-          <h3 className="header">Book Title</h3>
-          <div className="card horizontal cardfave">
-            <div className="card-image">
-              <img src="https://upload.wikimedia.org/wikipedia/en/thumb/6/6f/Stephen_King_Misery_cover.jpg/220px-Stephen_King_Misery_cover.jpg" />
-            </div>
-            <div className="card-stacked">
-              <div className="card-content">
-                <p>Plot: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde nesciunt minus ab animi autem sed modi perspiciatis a, deserunt magnam, provident ut nam quis minima aspernatur repudiandae iusto quam fugit.</p>
-                <ul>
-                  <li>Author: Stephen King</li>
-                  <li>Publisher: Viking</li>
-                  <li>Genre: Psychological Horror</li>
-                  <li>Date Published: 1987</li>
-                  <li>Pages: 420</li>
-                </ul>
-                <span>Rating: 5</span>
-              </div>
-              <div className="card-action">
-                <a>This is a link</a>
+      <div className="card-container">
+        {this.props.googleBooks.map((book, index)=>{
+          return (
+            <div className="col s12 m7">
+              <h3 className="header">{book.volumeInfo.title}</h3>
+              {book.volumeInfo.subtitle ? <h5>{book.volumeInfo.subtitle}</h5> : ''}
+              <div className="card horizontal cardfave">
+                <div className="card-image">
+                  <img src={book.volumeInfo.imageLinks.thumbnail} />
+                </div>
+                <div className="card-stacked">
+                  <div className="card-content">
+                    <ul>
+                      <li>Author: {book.volumeInfo.authors}</li>
+                      <li>Publisher: {book.volumeInfo.publisher}</li>
+                      <li>Genre: {book.volumeInfo.categories[0]}</li>
+                      <li>Publish Date: {book.volumeInfo.publishedDate}</li>
+                      <li>Pages: {book.volumeInfo.pageCount}</li>
+                      <li>Google Rating: {book.volumeInfo.averageRating}</li>
+                    </ul>
+                    <p>{book.volumeInfo.description}</p>
+                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">add</i></a>
+                  </div>
+                  <div className="card-action">
+                    <a href={book.volumeInfo.canonicalVolumeLink}>Google</a>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="col s12 m7">
-          <h3 className="header">Book Title</h3>
-          <div className="card horizontal cardfave">
-            <div className="card-image">
-              <img src="https://upload.wikimedia.org/wikipedia/en/thumb/6/6f/Stephen_King_Misery_cover.jpg/220px-Stephen_King_Misery_cover.jpg" />
-            </div>
-            <div className="card-stacked">
-              <div className="card-content">
-                <p>Plot: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde nesciunt minus ab animi autem sed modi perspiciatis a, deserunt magnam, provident ut nam quis minima aspernatur repudiandae iusto quam fugit.</p>
-                <ul>
-                  <li>Author: Stephen King</li>
-                  <li>Publisher: Viking</li>
-                  <li>Genre: Psychological Horror</li>
-                  <li>Date Published: 1987</li>
-                  <li>Pages: 420</li>
-                </ul>
-                <span>Rating: 5</span>
-              </div>
-              <div className="card-action">
-                <a>This is a link</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* End Demo */}
+          )
+        })}
       </div>
     )
   }
@@ -85,26 +42,49 @@ class SearchResult extends React.Component {
 class Home extends React.Component {
   constructor(props) {
     super(props)
+    this.queryBooks = this.queryBooks.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
     this.state = {
-      hideResult: true
+      query: '',
+      googleBooks: []
     }
   }
 
-  toggleHidden() {
-    event.preventDefault()
+  queryBooks(query) {
+    fetch('https://www.googleapis.com/books/v1/volumes?q='+ query).then((response)=>{
+      response.json().then((data)=>{
+        this.setState({
+          // googleBooks: data.items[0].volumeInfo
+          googleBooks: data.items
+        })
+        console.log(this.state.googleBooks);
+        console.log(this.state.googleBooks[0].volumeInfo);
+      })
+    }).catch((error)=>console.log(error))
+  }
+
+  handleChange(event) {
+    // console.log(event.target.value);
     this.setState({
-      hideResult: !this.state.hideResult
+      query: event.target.value
     })
+  }
+
+  handleSubmit(event) {
+    // console.log(event);
+    event.preventDefault()
+    this.queryBooks(this.state.query)
   }
 
   render() {
     return (
       <section className="search">
-        <form>
-          <input type="text" placeholder="Search Google Books"/>
-          <input onClick={this.toggleHidden.bind(this)} className="btn waves-effect waves-light" type="submit" name="action" />
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange} type="text" placeholder="Search Google Books"/>
+          <input className="btn waves-effect waves-light" type="submit" name="action" />
         </form>
-        {!this.state.hideResult && <SearchResult />}
+        <SearchResult queryBooks={()=>this.state.queryBooks(this.state.query)} googleBooks={this.state.googleBooks} />
       </section>
 
     )
