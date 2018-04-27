1ee137ad47b347414568dabd4a840cfedce3f27f
tnangel1984 <tnangel1984123@gmail.com>
2018-04-26 22:24:46 -0400

    Update method built. Not Working. Create method Tested. Working. Starting toggle method.

--- public/js/app.js
+++ public/js/app.js
@@ -24,30 +24,49 @@ class BookForm extends React.Component{
         }
     }
 
+    componentDidMount(){
+        if(this.props.book){
+            this.setState({
+                title:this.props.book.title,
+                author:this.props.book.author,
+                publisher:this.props.book.publisher,
+                date_published:this.props.book.date_published,
+                description:this.props.book.description,
+                cover_art:this.props.book.cover_art,
+                id:this.props.book.id
+            })
+        }
+    }
+
 
     formChange(event){
-        console.log(event.target.id);
-        console.log(event.target.value);
+        // console.log(event.target.id);
+        // console.log(event.target.value);
 
         this.setState({[event.target.id]:event.target.value})
     }
 
     formSubmit(event){
-        // event.target.preventDefault();
-        // this.setState({toggleState:!toggleState})
+        event.preventDefault();
+        this.props.submitDB(this.state);
     }
 
     render(){
-        console.log(this);
+    console.log(this.props.book);
         return <div>
         <h3>Testing BookForm</h3>
-        <form>
-            <input onChange={()=>this.formChange(event)} type="text"  placeholder="title" id="title"/> <br/>
-            <input type="text"  placeholder="author"  id="author"/> <br/>
-            <input type="text"  placeholder="publisher"  id ="publisher"/> <br/>
-            <input type="text"  placeholder="date_published" id="date_published"/> <br/>
-            <input type="text"  placeholder="description"  id="description"/>  <br/>
-            <input type="text"  placeholder="cover_art" id="cover_art"/> <br/>
+        <form  onSubmit={()=>this.formSubmit(event)}>
+            <input onChange={()=>this.formChange(event)}  value ={this.state.title} type="text"  placeholder="title" id="title"/> <br/>
+
+            <input onChange={()=>this.formChange(event)}  value ={this.state.author} type="text"  placeholder="author"  id="author"/> <br/>
+
+            <input onChange={()=>this.formChange(event)}  value ={this.state.publisher} type="text"  placeholder="publisher"  id ="publisher"/> <br/>
+
+            <input onChange={()=>this.formChange(event)}  value ={this.state.date_published} type="text"  placeholder="date_published" id="date_published"/> <br/>
+
+            <input onChange={()=>this.formChange(event)}  value ={this.state.description} type="text"  placeholder="description"  id="description"/>  <br/>
+
+            <input onChange={()=>this.formChange(event)}  value ={this.state.cover_art} type="text"  placeholder="cover_art" id="cover_art"/> <br/>
 
             <input type="Submit" value="Add Book" />
         </form>
@@ -59,7 +78,8 @@ class BookForm extends React.Component{
 
 class Book extends React.Component{
     render() {
-
+{/*console.log(this.props.book);
+console.log(this.props.submitDB);*/}
         return <div>
             <h3>Testing Book</h3>
             <h3>{this.props.book.title}</h3>
@@ -73,6 +93,12 @@ class Book extends React.Component{
                 <li>{this.props.book.date_published} </li>
             </ul>
 
+            <BookForm
+                book= {this.props.book}
+                submitDB = {this.props.submitDB}
+            >
+
+            </BookForm>
         </div>
     }
 }
@@ -105,18 +131,24 @@ class GoogleBooks extends React.Component{
         this.queryBooks = this.queryBooks.bind(this)
         this.getBooks = this.getBooks.bind(this)
         this.getBook = this.getBook.bind(this)
-        this.addBooks = this.addBooks.bind(this)
-        this.deleteBook = this.deleteBook.bind(this)
         this.createBook =this.createBook.bind(this)
+        this.addBookDB = this.addBookDB.bind(this)
+        this.updateBookDB=this.updateBookDB.bind(this)
+        this.deleteBook = this.deleteBook.bind(this)
+        this.toggleState=this.toggleState.bind(this)
+
         // this.formSubmit =this.formSubmit.bind(this)
         // this.formChange= this.formChange.bind(this)
 
-        this.state ={query:'harry+potter', foundBooks:[], selectedBook:{}, toggleState:false}
+        this.state ={query:'harry+potter', foundBooks:[], selectedBook:{}, toggleState:false,
+                        bookListVisible: true, bookVisible:true, createFormVisible: true, editFormVisible:true
+                    }
     }
 
     componentDidMount(){
         {this.queryBooks(this.state.query)}
         this.getBooks()
+
     }
 
 //======================  GOOGLE API ====================================//
@@ -151,25 +183,43 @@ class GoogleBooks extends React.Component{
 
 
     createBook(book){
+        console.log("createBook executed");
         const updatedBooks = this.state.foundBooks
-        updatedPeople.unshift(book)
+        updatedBooks.unshift(book)
         this.setState({foundBooks: updatedBooks})
 
     }
 
-    addBooks(){
-        fetch('/books', {body: JSON.stringify(), method: 'POST',
+    addBookDB(book){
+        fetch('/books', {body: JSON.stringify(book), method: 'POST',
                     headers:
                         {
                             'Accept': 'application/json, text/plain, */*',
                             'Content-Type': 'application/json'
                         }
     })
-    .then(response=>console.log(response)).catch(error=>console.log(error))
+    .then(response=>{return response.json()})
+    .then(response=>this.createBook(response))
+    .catch(error=>console.log(error))
     }
 
 
 
+
+    updateBookDB(book){
+        console.log("updatebook executed");
+        console.log(book);
+        // fetch('/books/'+ book.id, {body:JSON.stringify(book), method:'PUT',
+        //     headers:
+        //         {
+        //             'Accept':'application/json, text/plain, */*',
+        //             'Content-Type': 'application/json'
+        //         }
+        // })
+        // .then(response=> response.json())
+        // .then(updatedbook=>{this.getPeople()})
+        // .catch(error=>{console.log(error)})
+    }
 
     deleteBook(book, index){
         fetch('/books/'+ book.id, {method:'DELETE'})
@@ -182,20 +232,30 @@ class GoogleBooks extends React.Component{
         )
     }
 
+    toggleState(){
+
+    }
 // *******************************************
     render(){
 
         return( <div>
              <h1> HELLO</h1>
              <CustomBookAPI/>
+
              <BookList
                  books={this.state.foundBooks}
                  getBook={this.getBook}
             />
+
              <Book
                  book ={this.state.selectedBook}
+                 submitDB={this.updateBookDB}
              />
-             <BookForm />
+
+             <BookForm
+                create= {this.createBook}
+                submitDB ={this.addBookDB}
+             />
          </div>)
     }
 
