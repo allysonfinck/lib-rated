eaeaa4dbb620cf63ca2ec1a3e67c2af11b6a84b6
Eric Sanchez <bluemojo82@gmail.com>
2018-04-27 01:07:56 -0400

    created and defined home page

--- public/js/home.js
+++ public/js/home.js
@@ -3,13 +3,86 @@ class SearchResult extends React.Component {
   render() {
     return (
       <div>
-        <h1>results!</h1>
+        {/* Demo Result */}
+        <div className="col s12 m7">
+          <h3 className="header">Book Title</h3>
+          <div className="card horizontal cardfave">
+            <div className="card-image">
+              <img src="https://upload.wikimedia.org/wikipedia/en/thumb/6/6f/Stephen_King_Misery_cover.jpg/220px-Stephen_King_Misery_cover.jpg" />
+            </div>
+            <div className="card-stacked">
+              <div className="card-content">
+                <p>Plot: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde nesciunt minus ab animi autem sed modi perspiciatis a, deserunt magnam, provident ut nam quis minima aspernatur repudiandae iusto quam fugit.</p>
+                <ul>
+                  <li>Author: Stephen King</li>
+                  <li>Publisher: Viking</li>
+                  <li>Genre: Psychological Horror</li>
+                  <li>Date Published: 1987</li>
+                  <li>Pages: 420</li>
+                </ul>
+                <span>Rating: 5</span>
+              </div>
+              <div className="card-action">
+                <a>This is a link</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col s12 m7">
+          <h3 className="header">Book Title</h3>
+          <div className="card horizontal cardfave">
+            <div className="card-image">
+              <img src="https://upload.wikimedia.org/wikipedia/en/thumb/6/6f/Stephen_King_Misery_cover.jpg/220px-Stephen_King_Misery_cover.jpg" />
+            </div>
+            <div className="card-stacked">
+              <div className="card-content">
+                <p>Plot: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde nesciunt minus ab animi autem sed modi perspiciatis a, deserunt magnam, provident ut nam quis minima aspernatur repudiandae iusto quam fugit.</p>
+                <ul>
+                  <li>Author: Stephen King</li>
+                  <li>Publisher: Viking</li>
+                  <li>Genre: Psychological Horror</li>
+                  <li>Date Published: 1987</li>
+                  <li>Pages: 420</li>
+                </ul>
+                <span>Rating: 5</span>
+              </div>
+              <div className="card-action">
+                <a>This is a link</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col s12 m7">
+          <h3 className="header">Book Title</h3>
+          <div className="card horizontal cardfave">
+            <div className="card-image">
+              <img src="https://upload.wikimedia.org/wikipedia/en/thumb/6/6f/Stephen_King_Misery_cover.jpg/220px-Stephen_King_Misery_cover.jpg" />
+            </div>
+            <div className="card-stacked">
+              <div className="card-content">
+                <p>Plot: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde nesciunt minus ab animi autem sed modi perspiciatis a, deserunt magnam, provident ut nam quis minima aspernatur repudiandae iusto quam fugit.</p>
+                <ul>
+                  <li>Author: Stephen King</li>
+                  <li>Publisher: Viking</li>
+                  <li>Genre: Psychological Horror</li>
+                  <li>Date Published: 1987</li>
+                  <li>Pages: 420</li>
+                </ul>
+                <span>Rating: 5</span>
+              </div>
+              <div className="card-action">
+                <a>This is a link</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* End Demo */}
       </div>
     )
   }
 }
 
-class Search extends React.Component {
+class Home extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -26,17 +99,14 @@ class Search extends React.Component {
 
   render() {
     return (
-      <form>
-        <input type="text" placeholder="Search Google Books"/>
-        <input onClick={this.toggleHidden.bind(this)} className="btn waves-effect waves-light" type="submit" name="action" />
+      <section className="search">
+        <form>
+          <input type="text" placeholder="Search Google Books"/>
+          <input onClick={this.toggleHidden.bind(this)} className="btn waves-effect waves-light" type="submit" name="action" />
+        </form>
         {!this.state.hideResult && <SearchResult />}
-      </form>
+      </section>
 
     )
   }
 }
-
-ReactDOM.render(
-  <Search />,
-  document.querySelector('.search')
-)
